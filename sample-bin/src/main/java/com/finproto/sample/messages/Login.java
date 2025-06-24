b0a01bbce24e549799f4467d8edb9d6153d9e0c8
xinchen <283235702@qq.com>
2025-06-24 11:18:18 +0800

    chore#format by prettier

--- sample-bin/src/main/java/com/finproto/sample/messages/Login.java
+++ sample-bin/src/main/java/com/finproto/sample/messages/Login.java
@@ -7,34 +7,37 @@ import com.finproto.codec.BinaryCodec;
 import io.netty.buffer.ByteBuf;
 import io.netty.util.internal.StringUtil;
 
-public class Login implements BinaryCodec{
-    
+public class Login implements BinaryCodec {
+
     private String username;
     private String password;
 
     public String getUsername() {
         return username;
     }
+
     public void setUsername(String username) {
         this.username = username;
     }
+
     public String getPassword() {
         return password;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
-    
+
     @Override
     public void encode(ByteBuf byteBuf) {
-        if (StringUtil.isNullOrEmpty(this.username)){
+        if (StringUtil.isNullOrEmpty(this.username)) {
             byteBuf.writeShort(0);
         } else {
             byte[] bytes = this.username.getBytes(StandardCharsets.UTF_8);
             byteBuf.writeShort(bytes.length);
             byteBuf.writeBytes(bytes);
         }
-        if (StringUtil.isNullOrEmpty(this.password)){
+        if (StringUtil.isNullOrEmpty(this.password)) {
             byteBuf.writeShort(0);
         } else {
             byte[] bytes = this.password.getBytes(StandardCharsets.UTF_8);
@@ -47,11 +50,11 @@ public class Login implements BinaryCodec{
     public void decode(ByteBuf byteBuf) {
         short len = byteBuf.readShort();
         if (len > 0) {
-            this.username = byteBuf.readCharSequence(len, StandardCharsets.UTF_8).toString();;
+            this.username = byteBuf.readCharSequence(len, StandardCharsets.UTF_8).toString();
         }
         len = byteBuf.readShort();
         if (len > 0) {
-            this.password = byteBuf.readCharSequence(len, StandardCharsets.UTF_8).toString();;
+            this.password = byteBuf.readCharSequence(len, StandardCharsets.UTF_8).toString();
         }
     }
 
@@ -85,10 +88,10 @@ public class Login implements BinaryCodec{
             return false;
         return true;
     }
-    
+
     @Override
     public String toString() {
         return "Login [username=" + username + ", password=" + password + "]";
     }
-    
+
 }

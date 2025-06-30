f5855e218138085b3c876275da6f1f4eca5544ee
xinchen <xinchentechnote@163.com>
2025-06-30 21:59:04 +0800

    feat:re gen sample bin

--- sample-bin/src/main/java/com/finproto/sample/messages/NestedPacket.java
+++ sample-bin/src/main/java/com/finproto/sample/messages/NestedPacket.java
@@ -1,160 +1,183 @@
 package com.finproto.sample.messages;
-
-import com.finproto.codec.BinaryCodec;
-import io.netty.buffer.ByteBuf;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.function.Supplier;
+import com.finproto.codec.BinaryCodec;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.util.internal.StringUtil;
+
 
 public class NestedPacket implements BinaryCodec {
-  private List<SubPacket> subPacket;
-  private InerPacket inerPacket;
+    private SubPacket subPacket;
+    private List<SubPacket> subPacketList;
+    private InerPacket inerPacket;
 
-  public List<SubPacket> getSubPacket() {
-    return this.subPacket;
-  }
-
-  public void setSubPacket(List<SubPacket> subPacket) {
-    this.subPacket = subPacket;
-  }
-
-  public InerPacket getInerPacket() {
-    return this.inerPacket;
-  }
-
-  public void setInerPacket(InerPacket inerPacket) {
-    this.inerPacket = inerPacket;
-  }
-
-  @Override
-  public void encode(ByteBuf byteBuf) {
-    if (null == this.subPacket || this.subPacket.size() == 0) {
-      byteBuf.writeShort(0);
-    } else {
-      byteBuf.writeShortLE((short) this.subPacket.size());
-      for (int i = 0; i < this.subPacket.size(); i++) {
-        this.subPacket.get(i).encode(byteBuf);
-      }
+    public SubPacket getSubPacket() {
+        return this.subPacket;
     }
-    this.inerPacket.encode(byteBuf);
-  }
-
-  @Override
-  public void decode(ByteBuf byteBuf) {
-    short subPacketSize = byteBuf.readShortLE();
-    if (subPacketSize > 0) {
-      this.subPacket = new ArrayList<>();
-      for (int i = 0; i < subPacketSize; i++) {
-        SubPacket subPacket_ = new SubPacket();
-        subPacket_.decode(byteBuf);
-        this.subPacket.add(subPacket_);
-      }
+    
+    
+    public void setSubPacket(SubPacket subPacket) {
+        this.subPacket = subPacket;
     }
-    if (null == this.inerPacket) {
-      this.inerPacket = new InerPacket();
+    
+    public List<SubPacket> getSubPacketList() {
+        return this.subPacketList;
     }
-    this.inerPacket.decode(byteBuf);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(subPacket, inerPacket);
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj) {
-      return true;
+    
+    
+    public void setSubPacketList(List<SubPacket> subPacketList) {
+        this.subPacketList = subPacketList;
     }
-    if (null == obj || getClass() != obj.getClass()) {
-      return false;
+    
+    public InerPacket getInerPacket() {
+        return this.inerPacket;
     }
-    NestedPacket orther_ = (NestedPacket) obj;
-    return Objects.equals(subPacket, orther_.subPacket)
-        && Objects.equals(inerPacket, orther_.inerPacket);
-  }
-
-  @Override
-  public String toString() {
-    return "NestedPacket ["
-        + "subPacket="
-        + this.subPacket
-        + ", inerPacket="
-        + this.inerPacket
-        + "]";
-  }
-
-  public static class InerPacket implements BinaryCodec {
-    private int fieldU32;
-    private List<Short> fieldI16List;
-
-    public int getFieldU32() {
-      return this.fieldU32;
+    
+    
+    public void setInerPacket(InerPacket inerPacket) {
+        this.inerPacket = inerPacket;
     }
-
-    public void setFieldU32(int fieldU32) {
-      this.fieldU32 = fieldU32;
-    }
-
-    public List<Short> getFieldI16List() {
-      return this.fieldI16List;
-    }
-
-    public void setFieldI16List(List<Short> fieldI16List) {
-      this.fieldI16List = fieldI16List;
-    }
+    
 
     @Override
     public void encode(ByteBuf byteBuf) {
-      byteBuf.writeIntLE(this.fieldU32);
-      if (null == this.fieldI16List || this.fieldI16List.size() == 0) {
-        byteBuf.writeShort(0);
-      } else {
-        byteBuf.writeShortLE((short) this.fieldI16List.size());
-        for (int i = 0; i < this.fieldI16List.size(); i++) {
-          byteBuf.writeShortLE(this.fieldI16List.get(i));
+        this.subPacket.encode(byteBuf);
+        if (null == this.subPacketList || this.subPacketList.size() == 0) {
+            byteBuf.writeShort(0);
+        } else {
+            byteBuf.writeShortLE((short) this.subPacketList.size());
+            for (int i = 0; i < this.subPacketList.size(); i++) {
+                this.subPacketList.get(i).encode(byteBuf);
+            }
         }
-      }
+        this.inerPacket.encode(byteBuf);
     }
+    
 
     @Override
     public void decode(ByteBuf byteBuf) {
-      this.fieldU32 = byteBuf.readIntLE();
-      short fieldI16ListSize = byteBuf.readShortLE();
-      if (fieldI16ListSize > 0) {
-        this.fieldI16List = new ArrayList<>();
-        for (int i = 0; i < fieldI16ListSize; i++) {
-          this.fieldI16List.add(byteBuf.readShortLE());
+        if (null == this.subPacket) {
+            this.subPacket = new SubPacket();
         }
-      }
+        this.subPacket.decode(byteBuf);
+    short subPacketListSize = byteBuf.readShortLE();
+        if(subPacketListSize > 0) {
+        this.subPacketList = new ArrayList<>();
+        for(int i=0;i<subPacketListSize;i++) {
+        SubPacket subPacketList_ = new SubPacket();subPacketList_.decode(byteBuf);this.subPacketList.add(subPacketList_);
+        }
+        }
+        if (null == this.inerPacket) {
+            this.inerPacket = new InerPacket();
+        }
+        this.inerPacket.decode(byteBuf);
     }
+    
 
     @Override
     public int hashCode() {
-      return Objects.hash(fieldU32, fieldI16List);
+        return Objects.hash(subPacket, subPacketList, inerPacket);
     }
-
+    
     @Override
     public boolean equals(Object obj) {
-      if (this == obj) {
-        return true;
-      }
-      if (null == obj || getClass() != obj.getClass()) {
-        return false;
-      }
-      InerPacket orther_ = (InerPacket) obj;
-      return Objects.equals(fieldU32, orther_.fieldU32)
-          && Objects.equals(fieldI16List, orther_.fieldI16List);
+        if(this == obj) {
+            return true;
+        }
+        if(null == obj || getClass() != obj.getClass()) {
+            return false;
+        }
+        NestedPacket orther_ = (NestedPacket) obj;
+        return Objects.equals(subPacket, orther_.subPacket) && Objects.equals(subPacketList, orther_.subPacketList) && Objects.equals(inerPacket, orther_.inerPacket);
     }
+    
 
     @Override
     public String toString() {
-      return "InerPacket ["
-          + "fieldU32="
-          + this.fieldU32
-          + ", fieldI16List="
-          + this.fieldI16List
-          + "]";
+        return "NestedPacket [" + "subPacket=" + this.subPacket + ", subPacketList=" + this.subPacketList + ", inerPacket=" + this.inerPacket + "]";
     }
-  }
+    
+    public static class InerPacket implements BinaryCodec {
+        private int fieldU32;
+        private List<Short> fieldI16List;
+    
+        public int getFieldU32() {
+            return this.fieldU32;
+        }
+        
+        
+        public void setFieldU32(int fieldU32) {
+            this.fieldU32 = fieldU32;
+        }
+        
+        public List<Short> getFieldI16List() {
+            return this.fieldI16List;
+        }
+        
+        
+        public void setFieldI16List(List<Short> fieldI16List) {
+            this.fieldI16List = fieldI16List;
+        }
+        
+    
+        @Override
+        public void encode(ByteBuf byteBuf) {
+            byteBuf.writeIntLE(this.fieldU32);
+            if (null == this.fieldI16List || this.fieldI16List.size() == 0) {
+                byteBuf.writeShort(0);
+            } else {
+                byteBuf.writeShortLE((short) this.fieldI16List.size());
+                for (int i = 0; i < this.fieldI16List.size(); i++) {
+                    byteBuf.writeShortLE(this.fieldI16List.get(i));
+                }
+            }
+        }
+        
+    
+        @Override
+        public void decode(ByteBuf byteBuf) {
+            this.fieldU32 = byteBuf.readIntLE();
+        short fieldI16ListSize = byteBuf.readShortLE();
+            if(fieldI16ListSize > 0) {
+            this.fieldI16List = new ArrayList<>();
+            for(int i=0;i<fieldI16ListSize;i++) {
+            this.fieldI16List.add(byteBuf.readShortLE());
+            }
+            }
+        }
+        
+    
+        @Override
+        public int hashCode() {
+            return Objects.hash(fieldU32, fieldI16List);
+        }
+        
+        @Override
+        public boolean equals(Object obj) {
+            if(this == obj) {
+                return true;
+            }
+            if(null == obj || getClass() != obj.getClass()) {
+                return false;
+            }
+            InerPacket orther_ = (InerPacket) obj;
+            return Objects.equals(fieldU32, orther_.fieldU32) && Objects.equals(fieldI16List, orther_.fieldI16List);
+        }
+        
+    
+        @Override
+        public String toString() {
+            return "InerPacket [" + "fieldU32=" + this.fieldU32 + ", fieldI16List=" + this.fieldI16List + "]";
+        }
+        
+    
+    }
+
 }
--- sample-bin/src/test/java/com/finproto/sample/messages/EmptyPacketTest.java
+++ sample-bin/src/test/java/com/finproto/sample/messages/EmptyPacketTest.java
@@ -1,20 +1,22 @@
 package com.finproto.sample.messages;
-
-import static org.junit.Assert.*;
-
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import org.junit.Test;
 
-public class EmptyPacketTest {
-  @Test
-  public void testEncodeDecode() {
-    EmptyPacket original = new EmptyPacket();
+import java.util.Arrays;
 
-    ByteBuf buffer = Unpooled.buffer();
-    original.encode(buffer);
-    EmptyPacket decoded = new EmptyPacket();
-    decoded.decode(buffer);
-    assertEquals(original, decoded);
-  }
+import static org.junit.Assert.*;
+
+
+public class EmptyPacketTest {
+    @Test
+    public void testEncodeDecode() {
+        EmptyPacket original = new EmptyPacket();
+        
+        ByteBuf buffer = Unpooled.buffer();
+        original.encode(buffer);
+        EmptyPacket decoded = new EmptyPacket();
+        decoded.decode(buffer);
+        assertEquals(original, decoded);
+    }
 }
